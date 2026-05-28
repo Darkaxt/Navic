77741b651a9f6b8a3b66425ec6f00e5e39e01e64
Darkaxt <github.com.20240110@remaxku.eu>
2026-05-28 04:57:38 +0300

    Bump Android release to alpha67

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -20,8 +20,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 86
-		versionName = "v1.0.0-alpha66"
+		versionCode = 87
+		versionName = "v1.0.0-alpha67"
 
 		ndk {
 			abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
