344fc5d455a45acf8e79e72a6158797c40ea5e09
Darkaxt <github.com.20240110@remaxku.eu>
2026-05-28 22:28:54 +0300

    Bump version to v1.0.0-alpha89

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -32,8 +32,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 108
-		versionName = "v1.0.0-alpha88"
+		versionCode = 109
+		versionName = "v1.0.0-alpha89"
 
 		ndk {
 			abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
