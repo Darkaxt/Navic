d57fadc6d0a9682735b64181c1af45e56bd89d3a
Darkaxt <github.com.20240110@remaxku.eu>
2026-06-03 16:37:38 +0300

    chore(release): v1.0.10-gamma38

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,8 +46,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 225
-		versionName = "v1.0.10-gamma37"
+		versionCode = 226
+		versionName = "v1.0.10-gamma38"
 
 		ndk {
 			abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
