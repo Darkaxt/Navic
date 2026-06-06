fc9d359f9e8f04bc642c5a6508fc473148c3c242
Darkaxt <github.com.20240110@remaxku.eu>
2026-06-06 15:24:38 +0300

    chore: bump gamma60 release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,8 +46,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 246
-		versionName = "v1.0.10-gamma59"
+		versionCode = 247
+		versionName = "v1.0.10-gamma60"
 
 		ndk {
 			abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
