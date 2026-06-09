4e5b494680681bea1b53ba33e599374650871285
Darkaxt <github.com.20240110@remaxku.eu>
2026-06-10 01:42:26 +0300

    chore(release): bump epsilon13 version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,8 +46,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 287
-		versionName = "v1.0.10-epsilon12"
+		versionCode = 288
+		versionName = "v1.0.10-epsilon13"
 
 		ndk {
 			abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
