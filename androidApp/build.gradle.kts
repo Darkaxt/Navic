e923e3c692a66f970a9488fc1d62ce432466bff8
Darkaxt <github.com.20240110@remaxku.eu>
2026-06-10 05:17:32 +0300

    chore(release): bump epsilon20 version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,8 +46,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 294
-		versionName = "v1.0.10-epsilon19"
+		versionCode = 295
+		versionName = "v1.0.10-epsilon20"
 
 		ndk {
 			abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
