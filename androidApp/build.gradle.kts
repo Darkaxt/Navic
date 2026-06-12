08174bb7c075a2ab7aae14ef51c662e1d28d855e
Darkaxt <github.com.20240110@remaxku.eu>
2026-06-12 06:22:19 +0300

    chore(android): bump eta29 release version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,8 +46,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 361
-		versionName = "v1.0.11-eta28"
+		versionCode = 362
+		versionName = "v1.0.11-eta29"
 
 		ndk {
 			abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
