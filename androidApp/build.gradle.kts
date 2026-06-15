1794c51fe2c5eb6fe14daf5e02b96bc39aa49dd4
Darkaxt <github.com.20240110@remaxku.eu>
2026-06-15 08:20:13 +0300

    Bump Android reader candidate to eta65

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,8 +46,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 397
-		versionName = "v1.0.11-eta64"
+		versionCode = 398
+		versionName = "v1.0.11-eta65"
 
 		ndk {
 			abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
