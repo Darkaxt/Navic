242d207143fc1206a97f308a14a87a8dd2fb169f
Darkaxt <github.com.20240110@remaxku.eu>
2026-06-24 00:15:50 +0300

    chore: release eta85

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 417
-		versionName = "v1.0.11-eta84"
+		versionCode = 418
+		versionName = "v1.0.11-eta85"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
