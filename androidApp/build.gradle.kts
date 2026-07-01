b816241d5c2b1b992ac45e48c219fabb55f78be9
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-01 07:14:42 +0300

    Prepare theta29 reader release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 456
-		versionName = "v1.0.11-theta28"
+		versionCode = 457
+		versionName = "v1.0.11-theta29"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
