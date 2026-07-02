63c6263a4993bda1609a4a7fc3be79f8516f1a7f
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-02 23:33:33 +0300

    Prepare reader texture theta42 release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 469
-		versionName = "v1.0.11-theta41"
+		versionCode = 470
+		versionName = "v1.0.11-theta42"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
