ba75c8e631742eac8fc39bbe63f81a3292897f86
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-04 19:32:20 +0300

    Prepare theta57 Whispersync page-boundary release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 484
-		versionName = "v1.0.11-theta56"
+		versionCode = 485
+		versionName = "v1.0.11-theta57"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
