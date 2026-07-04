04ab325d684d2f8741e5d78062cd2a7ec34825b7
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-04 18:40:39 +0300

    Prepare theta56 Whispersync reader release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 483
-		versionName = "v1.0.11-theta55"
+		versionCode = 484
+		versionName = "v1.0.11-theta56"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
