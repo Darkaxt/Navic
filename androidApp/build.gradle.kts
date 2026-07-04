0ef4de99afae01c1b9eb4c19d64e2e7a0dd116a2
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-05 00:08:40 +0300

    Prepare theta60 artwork release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 487
-		versionName = "v1.0.11-theta59"
+		versionCode = 488
+		versionName = "v1.0.11-theta60"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
