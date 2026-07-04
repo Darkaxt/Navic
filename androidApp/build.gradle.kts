9f864175aeee4506935633898bbf7f23b1e7484c
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-04 21:09:28 +0300

    Prepare theta58 listening mode release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 485
-		versionName = "v1.0.11-theta57"
+		versionCode = 486
+		versionName = "v1.0.11-theta58"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
