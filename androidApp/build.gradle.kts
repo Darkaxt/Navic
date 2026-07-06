36f885f06519342262d0cd4f6d58251eb3bbaa20
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-06 23:36:17 +0300

    Prepare theta76 playback diagnostics release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 503
-		versionName = "v1.0.11-theta75"
+		versionCode = 504
+		versionName = "v1.0.11-theta76"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
