159ecad5119aeadfdaea04dfa24bdc22ecdbc9e9
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-08 18:40:09 +0300

    Prepare theta77 reader overlay release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 504
-		versionName = "v1.0.11-theta76"
+		versionCode = 505
+		versionName = "v1.0.11-theta77"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
