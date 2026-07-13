4f5dfbe7a46b51de022931098e958afc8bcb2f44
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-13 11:20:51 +0300

    chore(release): restore iota sequence at iota11

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 537
-		versionName = "v1.0.11-lambda1"
+		versionCode = 538
+		versionName = "v1.0.11-iota11"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
