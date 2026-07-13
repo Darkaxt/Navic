3eb70e2a718d77d1563ad31bf352e09b982dbb0a
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-14 01:49:50 +0300

    release(android): prepare iota21

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 547
-		versionName = "v1.0.11-iota20"
+		versionCode = 548
+		versionName = "v1.0.11-iota21"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
