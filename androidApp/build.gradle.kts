a70f98a87f8bcbd5e8e304c92e332ed08d5c7eb0
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-16 19:15:58 +0300

    release: prepare iota23 playback stability

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 549
-		versionName = "v1.0.11-iota22"
+		versionCode = 550
+		versionName = "v1.0.11-iota23"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
