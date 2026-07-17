f8a23bd1e7adba73b427223b02edf1c53cec93a9
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-17 12:40:37 +0300

    release: prepare iota24 PlayLikeCurl integration

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 550
-		versionName = "v1.0.11-iota23"
+		versionCode = 551
+		versionName = "v1.0.11-iota24"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
