0cc4a5c292823bcc17dc85b37d458b47015337b8
Darkaxt <github.com.20240110@remaxku.eu>
2026-07-20 00:23:13 +0300

    release: prepare iota25 music loading fixes

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "darkaxt.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 551
-		versionName = "v1.0.11-iota24"
+		versionCode = 552
+		versionName = "v1.0.11-iota25"
 		buildConfigField("boolean", "NAVIC_READER_DEV", "false")
 
 		ndk {
